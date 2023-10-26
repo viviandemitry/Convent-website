74ad3530b8ab63edc783d81e8e2c0b4f6aea9799
Vivian De Mitry <vivian090@gmail.com>
2023-10-26 10:17:09 -0300

    creating login page

--- src/views/Login/styles.js
+++ src/views/Login/styles.js
@@ -19,18 +19,24 @@ export const ContainerMain = styled.div`
 
 export const ContentContainer = styled.div`
     width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
     height: ${px2vw(550)};
     margin-top:${px2vw(200)};
     background-color: #FFF;
 `
 
 export const Title = styled.h2`
-    width: ${px2vw(800)};
-    margin-left: ${px2vw(150)};
-    margin-top: ${px2vw(50)};
+    width: ${px2vw(600)};
+    display: flex;
+    align-items: center;
+    justify-content: center;
     padding-bottom: ${px2vw(30)};
-    border-bottom: solid #5A9DCD;
-    color: #5A9DCD;
+    margin-top: ${px2vw(-100)};
+    border-bottom: solid #000D3F;
+    color: #000D3F;
 
     font-family: Bree Serif;
     font-size: ${px2vw(30)};
@@ -39,20 +45,51 @@ export const Title = styled.h2`
     line-height: normal;
 `
 
-export const ContactInfo = styled.div`
-    width: 100%;
-    margin-left: ${px2vw(100)};
+export const InputContainer = styled.form`
+    background: none;
     display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`
+export const InputContent = styled.input`
+    border-radius: ${px2vw(20)};
+    width: ${px2vw(300)};
+    height: ${px2vw(36)};
+    flex-shrink: 0;
+    margin-top: ${px2vw(30)};
+    margin-bottom: ${px2vw(48)};
+    text-align: center;
+
+    background: #FFF;
+    font-size: ${px2vw(18)};
+
+    @media ${device.mobileM}{
+        width: ${px2vw(600)};
+        height: ${px2vw(70)};
+
+        font-size: ${px2vw(30)};   
+    }
 `
 
-export const Text = styled.p`
-    /* width: ${px2vw(600)}; */
-    margin-left: ${px2vw(50)};
-    color: #011F66;
-
+export const InputLabel = styled.label`
+    display:flex;
+    justify-content: center;
+    flex-direction: column;
+    
+    color: #000D3F;
+    text-align: center;
     font-family: Segoe UI;
-    font-size: ${px2vw(23)};
+    font-size: ${px2vw(20)};
     font-style: normal;
     font-weight: 400;
     line-height: normal;
+    letter-spacing: ${px2vw(1.3)};
+    text-transform: capitalize;
+    background: none;
+
+    @media ${device.mobileM}{
+        font-size: ${px2vw(40)};
+        margin-top: ${px2vw(60)};
+    }
 `
--- src/views/Login/view.jsx
+++ src/views/Login/view.jsx
@@ -8,40 +8,18 @@ export default function Login() {
                 <Header />
                 <Styles.ContentContainer>
                     <Styles.Title>
-                        Contato
+                        Login
                     </Styles.Title>
-                    <Styles.ContactInfo>
-                        <Styles.Text>
-                            Telefone:
-                        </Styles.Text>
-                        <Styles.Text>
-                            (11)3673-0077
-                        </Styles.Text>
-                    </Styles.ContactInfo>
-                    <Styles.ContactInfo>
-                        <Styles.Text>
-                            Email:
-                        </Styles.Text>
-                        <Styles.Text>
-                            segredosdoconventosc@gmail.com
-                        </Styles.Text>
-                    </Styles.ContactInfo>
-                    <Styles.ContactInfo>
-                        <Styles.Text>
-                            Facebook:
-                        </Styles.Text>
-                        <Styles.Text>
-                            https://web.facebook.com/Segredosconvento/
-                        </Styles.Text>
-                    </Styles.ContactInfo>
-                    <Styles.ContactInfo>
-                        <Styles.Text>
-                            Endereço:
-                        </Styles.Text>
-                        <Styles.Text>
-                            Av. Dr. Arnaldo, 1492 - Sumaré, São Paulo CEP: 01255-000
-                        </Styles.Text>
-                    </Styles.ContactInfo>
+                    <Styles.InputContainer>
+                        <Styles.InputLabel>
+                            EMAIL
+                            <Styles.InputContent type="text" placeholder="Escreva seu e-mail" />
+                        </Styles.InputLabel>
+                        <Styles.InputLabel>
+                            PASSWORD 
+                            <Styles.InputContent type="password" placeholder="Escreva sua senha" />
+                        </Styles.InputLabel>
+                    </Styles.InputContainer>
                 </Styles.ContentContainer>
             </Styles.ContainerMain>
 
